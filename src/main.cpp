05a13680ac8eafce32f776dd9e19224346a4e8d1
honey the codewitch <gaztoast@gmail.com>
2022-07-23 10:26:03 -0700

    rely on external libs

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,20 @@
 
 using namespace arduino;
 using namespace sfx;
-using audio_t = i2s_external<-1,26,25,22,44100,16,i2s_channels::both>;
+using audio_t = i2s_external<-1,26,25,22,i2s_channels::both,true>;
 audio_t sound;
+SPIClass spi(VSPI);
 void setup() {
     Serial.begin(115200);
     SPIFFS.begin(false);
+    SD.begin(5,spi);
+    
     if(!sound.initialize()) {
         Serial.println("Unable to initialize I2S audio");
         while(1);
     }
     wav_file_source wav;
-    File file = SPIFFS.open("/demo.wav","rb");
+    File file = SD.open("/proto5.wav","rb");
     file_stream fs(file);
     sfx_result r = wav_file_source::open(&fs,&wav);
     if(r!=sfx_result::success) {
@@ -27,6 +30,7 @@ void setup() {
     transport trans;
     r=transport::create(&sound,&wav,&trans);
     wav.loop(true);
+    trans.volume(.10);
     if(r!=sfx_result::success) {
         Serial.print("Error initializing transport: ");
         Serial.println((int)r);
@@ -34,14 +38,9 @@ void setup() {
     }
     uint16_t samp[audio_t::dma_size];
     size_t written;
+
     while( r==sfx_result::success) {
-        size_t i;
-        if(audio_t::dma_size<(i=wav.read(samp,audio_t::dma_size))) {
-            Serial.print("read ");
-            Serial.println(i);
-            break;
-        }
-        i2s_write((i2s_port_t)0,samp,audio_t::dma_size,&written,portMAX_DELAY);
+        r=trans.update();
       
     }
     file.close();
